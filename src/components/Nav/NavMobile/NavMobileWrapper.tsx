9dc525b9f5547e759cf48ef2b71ed915cfd7cbf3
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-02-27 19:12:49 -0300

    Add delayed nav render

--- src/components/Nav/NavMobile/NavMobileWrapper.tsx
+++ src/components/Nav/NavMobile/NavMobileWrapper.tsx
@@ -6,7 +6,7 @@ export default function NavMobileWrapper({ children }: NavMobileWrapperProps) {
   return (
     <ul
       className={`
-    p-4 grid gap-3 fixed right-[.5%] top-[10%] 
+    p-4 grid gap-3 fixed right-[.5%] top-[22%] 
     bg-background-color border border-primary-color 
     shadow-md  rounded-md animeRight`}
     >
