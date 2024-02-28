171e43b2e6ac94d8401be75d89642e4b6e57b8ce
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-02-28 09:45:19 -0300

    new commit button

--- src/components/Nav/NavMobile/ButtonMobile.tsx
+++ src/components/Nav/NavMobile/ButtonMobile.tsx
@@ -13,16 +13,14 @@ export const ButtonMobile = ({
     <button
       {...props}
       className={`
-      border-2 border-primary-color p-6 rounded-full relative shadow-inner 
-      before:content[''] before:absolute before:bg-primary-color before:left-1/2 before:top-1/2
-      before:translate-x-[-50%] before:translate-y-[-50%] before:h-0.5 before:w-6
-      before:shadow-[0_8px_0_0_rgba(41,78,91,1),0_-8px_0_0_rgba(41,78,91,1)]
-      before:transition-all before:duration-500 before:ease-out
+      border-2 border-primary-color p-6 rounded-full relative shadow-inner before:content[''] before:absolute before:bg-primary-color before:left-1/2 before:top-1/2
+      before:translate-x-[-50%] before:translate-y-[-50%] before:h-0.5 before:w-6 before:shadow-[0_8px_0_0_rgba(41,78,91,1),0_-8px_0_0_rgba(41,78,91,1)] before:transition-all 
+      before:duration-500 before:ease-in-out
       ${
         isMenuMobileActive
           ? `before:rotate-90 before:w-[4px] before:h-1 before:rounded-full
           `
-          : null
+          : ""
       }
       `}
     />
