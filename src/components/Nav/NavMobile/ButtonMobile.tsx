5bc5bdaa01144d2902d83189b087325f49163412
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-02-28 09:42:50 -0300

    Testing button animation

--- src/components/Nav/NavMobile/ButtonMobile.tsx
+++ src/components/Nav/NavMobile/ButtonMobile.tsx
@@ -17,7 +17,7 @@ export const ButtonMobile = ({
       before:content[''] before:absolute before:bg-primary-color before:left-1/2 before:top-1/2
       before:translate-x-[-50%] before:translate-y-[-50%] before:h-0.5 before:w-6
       before:shadow-[0_8px_0_0_rgba(41,78,91,1),0_-8px_0_0_rgba(41,78,91,1)]
-      before:transition before:duration-500 before:ease-out
+      before:transition-all before:duration-500 before:ease-out
       ${
         isMenuMobileActive
           ? `before:rotate-90 before:w-[4px] before:h-1 before:rounded-full
