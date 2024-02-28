88eebf04eaf2de4b7d4f3eb357881d00a6e18101
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-02-28 09:52:22 -0300

    Hotfix type nav item

--- src/components/Nav/NavItem.tsx
+++ src/components/Nav/NavItem.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 interface NavItemProps {
   text: string;
   link: string;
-  isMobile: boolean;
+  isMobile?: boolean;
 }
 
 export default function NavItem({ text, link, isMobile }: NavItemProps) {
