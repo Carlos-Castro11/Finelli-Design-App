60f397ab6f7aa5ffb0b36ded7551e5fe8d370e36
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-02-28 10:04:41 -0300

    Add no icons menu test

--- src/components/Nav/NavMobile/index.tsx
+++ src/components/Nav/NavMobile/index.tsx
@@ -7,9 +7,6 @@ import { NavRoot } from "../NavRoot";
 // Constants
 import { ConstantsTitles, ConstantsUrls } from "@/constants";
 
-// Icon
-import Icon from "@/utils/Icon";
-
 export default function NavMobile() {
   const [mobileActive, setMobileActive] = React.useState(false);
   return (
@@ -21,25 +18,21 @@ export default function NavMobile() {
       {mobileActive ? (
         <NavRoot.Wrapper isMobile={true}>
           <NavRoot.Item
-            icon={<Icon name="Home" />}
             isMobile={true}
             link={ConstantsUrls.HOME_URL}
             text={ConstantsTitles.HOME_TITLE}
           />
           <NavRoot.Item
-            icon={<Icon name="Heart" />}
             isMobile={true}
             link={ConstantsUrls.ABOUT_URL}
             text={ConstantsTitles.ABOUT_TITLE}
           />
           <NavRoot.Item
-            icon={<Icon name="Book" />}
             isMobile={true}
             link={ConstantsUrls.PROJECTS_URL}
             text={ConstantsTitles.PROJECTS_TITLE}
           />
           <NavRoot.Item
-            icon={<Icon name="User" />}
             isMobile={true}
             link={ConstantsUrls.CONTACT_URL}
             text={ConstantsTitles.CONTACT_TITLE}
