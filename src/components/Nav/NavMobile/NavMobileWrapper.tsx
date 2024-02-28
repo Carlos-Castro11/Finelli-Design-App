d51607682cdbc23f579921797ab3d5b1a179ed87
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-02-28 10:08:58 -0300

    Add mobile wrapper

---
+++ src/components/Nav/NavMobile/NavMobileWrapper.tsx
@@ -0,0 +1,16 @@
+interface NavWrapperProps {
+  children: React.ReactNode;
+  isMobile?: boolean;
+}
+
+export default function NavMobileWrapper({ children }: NavWrapperProps) {
+  return (
+    <ul
+      className={`p-4 grid gap-3 fixed right-[.5%] top-[11%] 
+            bg-background-color border border-primary-color 
+            shadow-md  rounded-md animeRight`}
+    >
+      {children}
+    </ul>
+  );
+}
--- src/components/Nav/NavMobile/index.tsx
+++ src/components/Nav/NavMobile/index.tsx
@@ -7,6 +7,9 @@ import { NavRoot } from "../NavRoot";
 // Constants
 import { ConstantsTitles, ConstantsUrls } from "@/constants";
 
+// Icon
+import Icon from "@/utils/Icon";
+
 export default function NavMobile() {
   const [mobileActive, setMobileActive] = React.useState(false);
   return (
@@ -16,28 +19,32 @@ export default function NavMobile() {
         isMenuMobileActive={mobileActive}
       />
       {mobileActive ? (
-        <NavRoot.Wrapper isMobile={true}>
+        <NavMobileRoot.Wrapper isMobile={true}>
           <NavRoot.Item
+            icon={<Icon name="Home" />}
             isMobile={true}
             link={ConstantsUrls.HOME_URL}
             text={ConstantsTitles.HOME_TITLE}
           />
           <NavRoot.Item
+            icon={<Icon name="Heart" />}
             isMobile={true}
             link={ConstantsUrls.ABOUT_URL}
             text={ConstantsTitles.ABOUT_TITLE}
           />
           <NavRoot.Item
+            icon={<Icon name="Book" />}
             isMobile={true}
             link={ConstantsUrls.PROJECTS_URL}
             text={ConstantsTitles.PROJECTS_TITLE}
           />
           <NavRoot.Item
+            icon={<Icon name="User" />}
             isMobile={true}
             link={ConstantsUrls.CONTACT_URL}
             text={ConstantsTitles.CONTACT_TITLE}
           />
-        </NavRoot.Wrapper>
+        </NavMobileRoot.Wrapper>
       ) : null}
     </>
   );
