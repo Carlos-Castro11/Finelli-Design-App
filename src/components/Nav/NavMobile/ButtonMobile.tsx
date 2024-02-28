c46575db50838408e69a2912042c255e7e0933ba
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-02-28 09:56:09 -0300

    Test new button mobile

--- src/components/Nav/NavMobile/ButtonMobile.tsx
+++ src/components/Nav/NavMobile/ButtonMobile.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { ComponentProps } from "react";
 
 interface ButtonMobileProps extends ComponentProps<"button"> {
@@ -13,14 +12,16 @@ export const ButtonMobile = ({
     <button
       {...props}
       className={`
-      border-2 border-primary-color p-6 rounded-full relative shadow-inner before:content[''] before:absolute before:bg-primary-color before:left-1/2 before:top-1/2
-      before:translate-x-[-50%] before:translate-y-[-50%] before:h-0.5 before:w-6 before:shadow-[0_8px_0_0_rgba(41,78,91,1),0_-8px_0_0_rgba(41,78,91,1)] before:transition-all 
-      before:duration-500 before:ease-in-out
+      border-2 border-primary-color p-6 rounded-full relative shadow-inner 
+      before:content[''] before:absolute before:bg-primary-color before:left-1/2 before:top-1/2
+      before:translate-x-[-50%] before:translate-y-[-50%] before:h-0.5 before:w-6
+      before:shadow-[0_8px_0_0_rgba(41,78,91,1),0_-8px_0_0_rgba(41,78,91,1)]
+      before:transition before:duration-200 before:ease-out
       ${
         isMenuMobileActive
-          ? `before:rotate-90 before:w-[4px] before:h-1 before:rounded-full
+          ? `before:rotate-90 before:w-1 before:h-1 before:rounded-full 
           `
-          : ""
+          : null
       }
       `}
     />
