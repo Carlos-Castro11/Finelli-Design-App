47b1121ff79c5aba8673ee6ecb92526066a1df08
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-03-02 20:12:02 -0300

    Add new title

--- src/components/Sections/Projects/index.tsx
+++ src/components/Sections/Projects/index.tsx
@@ -1,12 +1,11 @@
 import Banner from "@/components/Banner.tsx";
-import BannerTitle from "@/components/Banner.tsx/BannerTitle";
+import TitleSection from "@/components/TitleSection";
 export default function Projects() {
   return (
     <>
-      <section id="projects" className="grid gap-10">
-        <Banner image="ferrari.jpg">
-          <BannerTitle text="Projetos" />
-        </Banner>
+      <section id="projects" className="mt-8">
+        <TitleSection title="projetos" />
+        <Banner image="ferrari.jpg" />
       </section>
     </>
   );
--- src/components/TitleSection/index.tsx
+++ src/components/TitleSection/index.tsx
@@ -4,7 +4,12 @@ interface TitleSectionProps {
 
 export default function TitleSection({ title }: TitleSectionProps) {
   return (
-    <h1 className="md:text-7xl text-5xl font-bold drop-shadow-text text-primary-color uppercase">
+    <h1
+      className={`md:text-7xl text-3xl font-bold text-primary-color uppercase relative
+      before:content[''] before:absolute before:bg-gradient-to-r from-transparent from-[1%] to-primary-color 
+      to-[20%] before:bottom-[45%] before:left-[35%] before:h-0.5 before:w-[65%]
+    `}
+    >
       {title}
     </h1>
   );
