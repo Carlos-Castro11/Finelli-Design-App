f9e6a41cef25117555013746f59a50caef06eb52
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-03-02 19:49:41 -0300

    Add second footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,17 +3,23 @@ import Icon from "@/utils/Icon";
 
 export default function Footer() {
   return (
-    <footer id="contact" className="h-80 bg-primary-color">
-      <h2 className="text-white">FOOTER</h2>
+    <footer id="contact">
+      <h2 className="text-white h-80 bg-primary-color">FOOTER</h2>
       {/* Second Footer */}
-      <div className="">
-        <p>© 2024 Todos os direitos Reservador para Giovana Finelli</p>
-        <p>
-          Desenvolvido por Carlos Castro{" "}
-          <span>
-            <Icon name="Code" />
-          </span>
-        </p>
+      <div className="bg-background-color p-4 shadow-inner text-xs sm:text-sm lg:text-base xl:text-lg">
+        <div className="grid grid-cols-2 divide-x divide-primary-color">
+          <div className="flex items-center justify-center">
+            <p>© 2024 Todos os direitos reservados para Giovana Finelli</p>
+          </div>
+          <div className="flex items-center justify-center">
+            <p className="flex gap-1 items-center">
+              Desenvolvido por Carlos Castro
+              <span className="text-primary-color">
+                <Icon name="Code" />
+              </span>
+            </p>
+          </div>
+        </div>
       </div>
     </footer>
   );
