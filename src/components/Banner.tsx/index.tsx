ab9edc968affabb4040573e6e9bf8d46a3e4a6ad
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-03-02 19:58:10 -0300

    Hotfix

--- src/components/Banner.tsx/index.tsx
+++ src/components/Banner.tsx/index.tsx
@@ -7,7 +7,7 @@ export default function Banner({ image, children }: BannerProps) {
   return (
     <div
       style={{ backgroundImage: `url(images/${image})` }}
-      className="mt-5 flex items-center justify-center h-28 sm:h-48 md:h-[22rem] bg-fixed bg-cover md:bg-center rounded shadow-large"
+      className="mt-5 flex items-center justify-center h-28 sm:h-48 md:h-[22rem] bg-fixed bg-cover bg-center rounded shadow-large"
     >
       {children}
     </div>
