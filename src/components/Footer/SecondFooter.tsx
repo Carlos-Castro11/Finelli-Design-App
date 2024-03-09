2a3af86395b00fc46d20b4fb2a228305896485da
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-03-09 19:48:12 -0300

    Add responsive second footer

--- src/components/Footer/SecondFooter.tsx
+++ src/components/Footer/SecondFooter.tsx
@@ -3,13 +3,14 @@ import Icon from "@/utils/Icon";
 
 export default function SecondFooter() {
   return (
-    <div className="bg-background-color p-4 shadow-inner text-[0.8rem] sm:text-sm lg:text-base xl:text-lg">
+    <div className="bg-background-color p-4 shadow-inner text-[0.8rem] sm:text-sm lg:text-base">
       <div
         className={`grid sm:grid-cols-2 gap-5
         relative after:absolute after:content['']
         after:bg-gradient-to-r from-transparent via-primary-color to-transparent
         after:h-[1px] after:w-full
         after:left-0 after:bottom-1/2
+        sm:after:h-full sm:after:w-[1px] sm:after:left-1/2 sm:after:bottom-0
         `}
       >
         <div className="flex items-center justify-center">
