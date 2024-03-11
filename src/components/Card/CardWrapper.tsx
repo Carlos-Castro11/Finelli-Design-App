1b0a3b2714ca5e6ef68ebe371959e79bb5e670b8
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-03-10 23:25:50 -0300

    Add hotfix for service section

--- src/components/Card/CardWrapper.tsx
+++ src/components/Card/CardWrapper.tsx
@@ -6,7 +6,7 @@ export default function CardWrapper({ children }: CardWrapperProps) {
   return (
     <div
       className={`bg-background-color border border-primary-color 
-      rounded-lg grid place-content-center shadow-large
+      rounded-lg grid place-content-center shadow-large xl:p-3
     `}
     >
       {children}
--- src/components/Nav/NavItem.tsx
+++ src/components/Nav/NavItem.tsx
@@ -24,7 +24,7 @@ export default function NavItem({
             ? "text-lg flex gap-2"
             : `p-0.5 relative text-base
           after:bg-primary-color after:content-[''] 
-          after:absolute after:h-0.5 after:w-0 after:bottom-0.5 after:left-1/2 
+          after:absolute after:h-0.5 after:w-0 after:bottom-[1px] after:left-1/2 
           after:duration-300 after:ease-in-out after:translate-x-[-50%]
           hover:after:w-full`
         }`}
