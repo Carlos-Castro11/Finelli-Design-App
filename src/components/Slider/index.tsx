c9450fb3f54e39172b9a3eebc7547744710eb027
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-03-15 10:59:58 -0300

    Add hotfix slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -12,7 +12,7 @@ export default function Slider() {
   const screenWidth = useMedia("(max-width: 1100px)");
 
   return (
-    <div className="mt-8">
+    <div className="lg:mt-8">
       {screenWidth ? (
         <SliderRoot.Content />
       ) : (
