a4cdaefec5d1b8b0d1475f92ad13c6ffeb8404b0
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-05-25 12:54:41 -0300

    Refactoring card

--- src/components/Sections/Services/index.tsx
+++ src/components/Sections/Services/index.tsx
@@ -1,31 +1,33 @@
 // Components
-import Card from "@/components/Card";
-import MainTitle from "@/components/MainTitle";
+import { CardRoot } from '@/components/Card/CardRoot';
+import DefaultText from '@/components/DefaultText';
+import SecondTitle from '@/components/SecondTitle';
+import MainTitle from '@/components/MainTitle';
 
 // Constants
-import { ConstantsTitles } from "@/constants";
+import { ConstantsTitles } from '@/constants';
 
 // Utils
-import Container from "@/utils/Container";
-import Icon from "@/utils/Icon";
+import Container from '@/utils/Container';
+import Icon from '@/utils/Icon';
 
 // Data
-import { ServicesData } from "@/Database/Services";
+import { ServicesData } from '@/Database/Services';
 
 export default function Services() {
   return (
-    <section id="services" className="mt-8">
+    <section id='services' className='mt-8'>
       <MainTitle title={ConstantsTitles.SERVICES_TITLE} />
       <Container>
-        <div className="grid lg:grid-cols-3 gap-8 p-1 lg:p-10">
+        <div className='grid lg:grid-cols-3 gap-8 p-1 lg:p-10'>
           {ServicesData.map((card) => (
-            <Card
-              isMiddleCard={card.isMiddleCard ? card.isMiddleCard : false}
-              key={card.id}
-              title={card.title}
-              text={card.text}
-              icon={<Icon name={card.iconName} size={55} />}
-            />
+            <CardRoot.Wrapper key={card.id} isMiddleCard={card.isMiddleCard}>
+              <CardRoot.Content>
+                <CardRoot.Icon icon={<Icon name={card.iconName} size={55} />} />
+                <SecondTitle title={card.title} />
+                <DefaultText text={card.text} />
+              </CardRoot.Content>
+            </CardRoot.Wrapper>
           ))}
         </div>
       </Container>
