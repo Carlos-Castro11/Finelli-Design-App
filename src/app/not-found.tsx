3f7db748491e6a1e71c929796a1389af35c73a5c
Carlos Castro <carloshenriquemg2020@gmail.com>
2024-05-26 15:31:14 -0300

    Add loading to app

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import MainTitle from "@/components/MainTitle"
+
+function NotFoundPage() {
+	return <div className="h-[51%] flex items-center justify-center">
+        <MainTitle title="404 - Página não encontrada"/> 
+    </div>
+}
+
+export default NotFoundPage
--- src/components/Sections/UniqueProject/index.tsx
+++ src/components/Sections/UniqueProject/index.tsx
@@ -1,41 +1,80 @@
-"use client";
+'use client';
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React from 'react';
 
 // Components
-import MainTitle from "@/components/MainTitle";
+import MainTitle from '@/components/MainTitle';
+import { Loading } from '@/components/Loading';
 
 // Interface
-import { ProjectsDataProps } from "@/Database/Projects";
+import { ProjectsDataProps } from '@/Database/Projects';
 
 // Hooks
-import useMedia from "@/hooks/useMedia";
-import { UniqueProjectImageRoot } from "@/components/UniqueProjectImages/UniqueProjectImageRoot";
-import SecondTitle from "@/components/SecondTitle";
-import DefaultText from "@/components/DefaultText";
+import useMedia from '@/hooks/useMedia';
+import { UniqueProjectImageRoot } from '@/components/UniqueProjectImages/UniqueProjectImageRoot';
+import SecondTitle from '@/components/SecondTitle';
+import DefaultText from '@/components/DefaultText';
 
 interface UniqueProjectProps {
   project: ProjectsDataProps;
 }
 
 export default function UniqueProject({ project }: UniqueProjectProps) {
-  const isGridWithTwoColumns = useMedia("(min-width: 1024px)");
+  const isGridWithTwoColumns = useMedia('(min-width: 1024px)');
+
   const [coverImage, setCoverImage] = React.useState<string>(
     project.image_cover
   );
+  const [isLoading, setIsloading] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsloading(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      <div className="grid lg:grid-cols-2 lg:container gap-6 lg:gap-0 lg:mt-7">
-        <div className="flex flex-col lg:flex-row">
-          {isGridWithTwoColumns ? null : <MainTitle title={project.title} />}
-          <img
-            className="lg:rounded shadow-large lg:max-w-[95%] object-cover"
-            src={`/images/${coverImage}`}
-            alt=""
-          />
-          {isGridWithTwoColumns ? null : (
-            <div className="lg:container lg:mx-0 mx-2">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className='grid lg:grid-cols-2 lg:container gap-6 lg:gap-0 lg:mt-7'>
+          <div className='flex flex-col lg:flex-row'>
+            {isGridWithTwoColumns ? null : <MainTitle title={project.title} />}
+            <img
+              className='lg:rounded shadow-large lg:max-w-[95%] object-cover'
+              src={`/images/${coverImage}`}
+              alt=''
+            />
+            {isGridWithTwoColumns ? null : (
+              <div className='lg:container lg:mx-0 mx-2'>
+                <UniqueProjectImageRoot.Wrapper>
+                  {project.images.map((image) => (
+                    <UniqueProjectImageRoot.Image
+                      onClick={() => setCoverImage(image)}
+                      image={image}
+                      key={image}
+                    />
+                  ))}
+                </UniqueProjectImageRoot.Wrapper>
+              </div>
+            )}
+          </div>
+          <div>
+            {/* <Banner image={project.image_cover}>
+            <BannerTitle text={project.title} />
+          </Banner> */}
+            {isGridWithTwoColumns ? (
+              <>
+                <MainTitle title={project.title} />
+              </>
+            ) : null}
+            <div className='lg:mt-5 mx-2 lg:mx-0'>
+              <SecondTitle title='Sobre o projeto' />
+              <DefaultText text={project.about} />
+            </div>
+            {isGridWithTwoColumns ? (
               <UniqueProjectImageRoot.Wrapper>
                 {project.images.map((image) => (
                   <UniqueProjectImageRoot.Image
@@ -45,35 +84,10 @@ export default function UniqueProject({ project }: UniqueProjectProps) {
                   />
                 ))}
               </UniqueProjectImageRoot.Wrapper>
-            </div>
-          )}
-        </div>
-        <div>
-          {/* <Banner image={project.image_cover}>
-            <BannerTitle text={project.title} />
-          </Banner> */}
-          {isGridWithTwoColumns ? (
-            <>
-              <MainTitle title={project.title} />
-            </>
-          ) : null}
-          <div className="lg:mt-5 mx-2 lg:mx-0">
-            <SecondTitle title="Sobre o projeto" />
-            <DefaultText text={project.about} />
+            ) : null}
           </div>
-          {isGridWithTwoColumns ? (
-            <UniqueProjectImageRoot.Wrapper>
-              {project.images.map((image) => (
-                <UniqueProjectImageRoot.Image
-                  onClick={() => setCoverImage(image)}
-                  image={image}
-                  key={image}
-                />
-              ))}
-            </UniqueProjectImageRoot.Wrapper>
-          ) : null}
         </div>
-      </div>
+      )}
     </>
   );
 }
